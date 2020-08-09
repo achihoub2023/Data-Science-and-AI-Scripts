ff1349b1a1db304f15ef81e3d56dc9bb8098676f
william-the-bastard <william-the-bastard@williamtardsAir.fios-router.home>
2020-08-09 03:57:26 -0400

    updates to makefile

---
+++ Motion_Planning/A.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+    int dimA,dimB;
+    cout << "Enter dimA: ";
+    cin >> dimA;
+    cout<<"Enter dimB: ";
+    cin >> dimB;
+
+    cout<< dimA;
+    return 0;
+}
